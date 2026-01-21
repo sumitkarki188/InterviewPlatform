bb9d1c8b89b13aca47d3b88fbb9e957f5f9b3898
Sumit Singh Karki <sumitkarki1114@gmail.com>
2026-01-21 19:28:27 +0530

    Add user upsert and delete functions in inngest.js

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({ id: "interview-platform" });
 
@@ -20,6 +21,11 @@ const syncUser = inngest.createFunction(
         };
 
         await User.create(newUser);
+
+        await upsertStreamUser({
+            id:newUser.clerkId.toString(),
+            name:newUser.profileImage,
+        });
         
         return { success: true, userId: id };
     }
@@ -33,6 +39,8 @@ const deleteUserFromDB = inngest.createFunction(
 
         const { id } = event.data;
         await User.deleteOne({ clerkId: id });
+
+        await deleteStreamUser(id.toString());
         
         return { success: true, deletedUserId: id };
     }
