65ca40d4f20fe7987555378ac753903012f5eb43
Sumit Singh Karki <sumitkarki1114@gmail.com>
2026-01-21 22:27:13 +0530

    Update env.js

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -1,15 +1,14 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-dotenv.config({quiet:true});
+dotenv.config();
 
-export const ENV={
-    PORT:process.env.PORT,
-    DB_URL:process.env.DB_URL,
-    NODE_ENV:process.env.NODE_ENV,
-    CLIENT_URL:process.env.CLIENT_URL,
-    INNGEST_EVENT_KEY:process.env.INNGEST_EVENT_KEY,
-    INNGEST_SIGNING_KEY:process.envINNGEST_SIGNING_KEY,
-    STREAM_API_KEY:process.env.STREAM_API_KEY,
-    STREAM_API_SECRET:process.env.STREAM_API_SECRET,
-
+export const ENV = {
+    PORT: process.env.PORT || 3000,
+    DB_URL: process.env.DB_URL,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    CLIENT_URL: process.env.CLIENT_URL || 'http://localhost:5173',
+    INNGEST_EVENT_KEY: process.env.INNGEST_EVENT_KEY,
+    INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY,
+    STREAM_API_KEY: process.env.STREAM_API_KEY,
+    STREAM_API_SECRET: process.env.STREAM_API_SECRET,
 }
